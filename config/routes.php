d9190bd9376a11c568036eafd98c9cc33dcef7c2
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2023-07-18 21:42:25 +0300

    p1/l7: implemented methods: 'matchRoute', 'toCamelCase'

--- config/routes.php
+++ config/routes.php
@@ -3,19 +3,19 @@
 use  YafrmCore\Classes\Router;
 
 // Админка
-Router::add('^admin/?&', [
+Router::add('^admin/?$', [
     'controller' => 'Main',
     'action' => 'index',
     'admin_prefix' => 'admin',
 ]);
 
-Router::add('^admin/?(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?&', ['admin_prefix' => 'admin']);
+Router::add('^admin/?(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$', ['admin_prefix' => 'admin']);
 
 
 // Фронт
-Router::add('^&', [
+Router::add('^$', [
     'controller' => 'Main',
     'action' => 'index',
 ]);
 
-Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?&');
+Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$');
--- vendor/yafrm/Classes/Router.php
+++ vendor/yafrm/Classes/Router.php
@@ -25,6 +25,52 @@ class Router
 
     public static function dispatch(string $uri)
     {
-        dump($uri);
+        if (self::matchRoute($uri)){
+            echo 'OK';
+        } else {
+            echo 'NOT OK';
+        }
+
+    }
+
+    public static function matchRoute(string $uri): bool
+    {
+        foreach (self::getRoutes() as $pattern => $route) {
+            if (preg_match("#$pattern#", $uri, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)){
+                        $route[$k] = $v;
+                    }
+                }
+
+                if (!array_key_exists('action', $route)) {
+                    $route['action'] = 'index';
+                }
+
+                if (!array_key_exists('admin_prefix', $route)) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+
+                $route['controller'] = self::toCamelCase($route['controller']);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function toCamelCase(string $name, bool $capitalizeFirstChar = true): string
+    {
+        $words = array_map('ucfirst', explode('-', $name));
+        $result = implode('', $words);
+
+        if (!$capitalizeFirstChar) {
+            $result = lcfirst($result);
+        }
+
+        return $result;
     }
 }
